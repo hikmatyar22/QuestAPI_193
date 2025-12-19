06453fa5ebe21abfe0cdff478756a15f98792826
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-19 10:09:51 +0700

    membuat data class data siswa yang digunakan untuk menyimpan dan mengelola data siswa, di mana DataSiswa berfungsi sebagai model data utama, UIStateSiswa untuk menyimpan kondisi tampilan aplikasi, dan DetailSiswa untuk menampung data siswa sementara saat proses input atau edit.

---
+++ app/src/main/java/com/example/praktikum8/modeldata/DataSiswa.kt
@@ -0,0 +1,28 @@
+package com.example.praktikum8.modeldata
+
+import kotlinx.serialization.Serializable
+import kotlin.Int
+@Serializable
+data class DataSiswa (
+    val id : Int,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
+)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa (
+    val id : Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+
+
+
+
