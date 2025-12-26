ddf62b7de6a01f03d599eedf80be090bb373daaa
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-26 19:05:32 +0700

    memodifikasi SiswaTopAppBar dengan terbaru menghapus import dan penggunaan resource stringResource untuk contentDescription serta membersihkan import ikon yang tidak digunakan, sehingga kode menjadi lebih sederhana dan rapi tanpa mengubah fungsi navigasi tombol kembali.

--- app/src/main/java/com/example/praktikum8/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/praktikum8/view/SiswaTopAppBar.kt
@@ -1,8 +1,8 @@
+
 package com.example.praktikum8.view
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -11,8 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.example.praktikum8.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,21 +19,17 @@ fun SiswaTopAppBar(
     canNavigateBack: Boolean,
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
-    navigateUp: () -> Unit = {}
-) {
-    CenterAlignedTopAppBar(
-        title = { Text(title) },
+    navigateUp:() -> Unit = {}
+){
+    CenterAlignedTopAppBar(title = { Text(title) },
         modifier = modifier,
         scrollBehavior = scrollBehavior,
         navigationIcon = {
-            if (canNavigateBack) {
+            if (canNavigateBack){
                 IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back)
-                    )
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null)
                 }
             }
-        }
-    )
+        })
 }
