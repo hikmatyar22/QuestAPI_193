4a226fd718915a7c0dfe5447cc8ffeb4d5d73309
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-26 18:14:01 +0700

    mengupdate home view model untuk digunakan untuk mengelola dan menampilkan status data siswa pada UI dengan tiga kondisi (Loading, Success, dan Error) melalui HomeViewModel yang mengambil data dari repository secara asynchronous menggunakan coroutine.

--- app/src/main/java/com/example/praktikum8/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/HomeViewModel.kt
@@ -11,30 +11,27 @@ import kotlinx.coroutines.launch
 import retrofit2.HttpException
 import java.io.IOException
 
-sealed interface StatusUiSiswa {
-    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
-    object Error : StatusUiSiswa
-    object Loading : StatusUiSiswa
+sealed interface StatusUiSiswa{
+    data class Success(val siswa: List<DataSiswa> = listOf()): StatusUiSiswa
+    object Error: StatusUiSiswa
+    object Loading: StatusUiSiswa
 }
 
-class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
-    ViewModel() {
-
+class HomeViewModel (private val repositoryDataSiswa: RepositoryDataSiswa):
+    ViewModel(){
     var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
         private set
-
     init {
         loadSiswa()
     }
-
-    fun loadSiswa() {
+    fun loadSiswa(){
         viewModelScope.launch {
             listSiswa = StatusUiSiswa.Loading
-            try {
-                listSiswa = StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
-            } catch (e: IOException) {
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
+            }catch (e: IOException){
                 StatusUiSiswa.Error
-            } catch (e: HttpException) {
+            }catch (e: HttpException){
                 StatusUiSiswa.Error
             }
         }
