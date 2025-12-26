fd3f70c8b810dbf7c1e726989b9ee28b7c29e126
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-12-26 18:05:56 +0700

    memnambahkan hapusSatuSiswa digunakan untuk menghapus data satu siswa berdasarkan ID, dan hasilnya berupa status berhasil atau gagal.

--- app/src/main/java/com/example/praktikum8/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/praktikum8/repositori/RepositoriDataSiswa.kt
@@ -8,7 +8,7 @@ interface RepositoryDataSiswa {
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
     suspend fun getSatuSiswa(id:Int) : DataSiswa
     suspend fun editSatuSiswa (id:Int,datasiswa: DataSiswa) : retrofit2.Response<Void>
-
+    suspend fun hapusSatuSiswa (id:Int) : retrofit2.Response<Void>
 
 }
 
